ba98eadf972724552de15c275f228655a92ca80a
Issayo5o <issa.alwazir@gmail.com>
2024-12-18 12:37:05 -0800

    Update CameraFollow.cs
    changing camera offset to make the player centered but lower

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float followSpeed = 2f; // How fast camera moves to target
-    public float yOffset = 1f; // Height of camera
+    public float yOffset = 2.84f; // Height of camera
     public Transform target; // Gives position of player
 
     
