736b96b46d93b1eeb3b7e17f9a396e9664a35ced
Nathanv2 <lnathan718@gmail.com>
2025-02-25 12:31:45 -0800

    Added a restart button, life system, game over method

--- Assets/Levels/Scripts/PlayerMovement.cs
+++ Assets/Levels/Scripts/PlayerMovement.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameOver gameOver;
+    public TextMeshProUGUI gameOverText;
+    public Button restart;
 
     private Rigidbody2D body;
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
     private Animator anim;
     private bool grounded;
+
+
+    public Image Red_Heart;
+    public Image Red_Heart2;
+    public Image Red_Heart3;
+
+    public Image Grey_Heart;    
+    public Image Grey_Heart2;
+    public Image Grey_Heart3;
+
+    public int number = 0;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -16,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -64,5 +82,36 @@ public class PlayerMovement : MonoBehaviour
         {
             grounded = true;
         }
+
+        if (collision.gameObject.CompareTag("Obstacle") && number == 0)
+        {
+            Red_Heart.gameObject.SetActive(false);
+            Grey_Heart.gameObject.SetActive(true);
+            number = 1;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle") && number == 1)
+        {
+            Red_Heart2.gameObject.SetActive(false);
+            Grey_Heart2.gameObject.SetActive(true);
+            number = 2;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle") && number == 2)
+        {
+            Red_Heart3.gameObject.SetActive(false);
+            Grey_Heart3.gameObject.SetActive(true);
+            number = 3;
+            gameOverText.gameObject.SetActive(true);
+            gameOver.EndGame();
+            //with this number you could probably add a gameover method
+        }
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1.0f;
+    }
+
+    
+
 }
